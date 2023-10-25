df288120d583f892c8682341d3928aeefea4fe34
Szyszka0 <alaszyszko175@gmail.com>
2023-10-25 14:09:58 +0200

    MEOWMEOW

---
+++ Lekcja1 Struktura programu/BezNazwy1.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+ using namespace std;
+ int main(){
+ 	short liczba = 2;
+ 	float zmiennoprzecinkowa = 3.532;
+ 	string tekst = "tekst";
+ 	char znak = 'A';
+ 	bool znakowy = false;
+ 	
+ 	
+ 	
+ 	cout << "Meow" << endl;
+ 	
+ 	cout << "liczba calkowita " << liczba;
+ 	cout << endl;
+ 	
+ 	string jakos;
+ 	cout << "Podaj jakis wyraz: ";
+ 	cin >> jakos;
+
+ 	cout << "Wprowadziles wartosc: " << jakos;
+ 	
+ 	return 0;
+ }
