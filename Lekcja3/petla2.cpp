abb68a1c69d9484938fc5d9d58710a6f24d78a49
Szyszka0 <alaszyszko175@gmail.com>
2023-11-09 15:03:14 +0100

    Komentarz

---
+++ Lekcja3/petla2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=1; i < 11; i++){
+        for(int j=0; j < 11; j++){
+            cout << i << " * " << j << " = " << i*j << endl;
+            
+          
+        }
+        cout << endl;
+    }
+}
