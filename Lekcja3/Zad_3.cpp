48ce732e393ebdb07da7df5b5e191184814e512a
Szyszka0 <alaszyszko175@gmail.com>
2023-11-16 15:00:35 +0100

    Komentarz

---
+++ Lekcja3/Zad_3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	short szerokosc, wysokosc;
+	cout << "Podaj szerokosc: ";
+	cin >> szerokosc;
+	cout << "Podaj wysokosc: ";
+	cin >> wysokosc;
+
+
+for(int i=0; i < wysokosc; i++){
+	for(int j=0; j < szerokosc; j++){
+		
+		if(i == 0 || i == wysokosc - 1)cout << "-";
+		else if(j == 0 || j == szerokosc -1)cout << "|";
+		else if((i+j)%2 == 0)
+		cout << "#";
+		else
+		cout << "*";
+	
+	}
+	cout << endl;
+}
+
+
+
+}
