d5539f97dadb955744c48d4a62616fe96155068e
Szyszka0 <alaszyszko175@gmail.com>
2023-11-30 15:02:03 +0100

    Komentarz

---
+++ Lekcja3/tablica zmienna.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int tablica [ 5 ];
+	cout << "Wprowadz 5 liczb: " << endl;
+	for(int i=0; i<5; i++){
+		cin >> tablica[ i ];
+		
+		
+	
+	
+	}
+	for(int i=0; i<5; i++){
+		cout << i;
+	}
+	
+
+}
