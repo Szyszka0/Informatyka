7154d0fe1caa9f79da7df6679d8c38346188feee
Szyszka0 <alaszyszko175@gmail.com>
2024-03-18 14:24:02 +0100

    meow

--- Struktury danych/zad2.cpp
+++ Struktury danych/zad2.cpp
@@ -21,22 +21,37 @@ float cena;
     { "Tytul7", "autor1", 1931, "Helion1", 29.54 },
 };
 int main(){
+	short wybor_rok;
+	float wybor_cena;
+	string wybor_wydawnictwo;
 	int wybor;
 	cout << "Witaj w ksiegarnii" << endl;
-	cout << "Po jakiej kategorii chcil bys wyszkuac ksiazke: " << endl << "1. Rok wydania " << endl << "2. Cena " << endl << "3. Wydawnictwo " << endl << "0. Wyjscie";
+	cout << "Po jakiej kategorii chcil bys wyszkuac ksiazke: " << endl << "1. Rok wydania " << endl << "2. Cena " << endl << "3. Wydawnictwo " << endl << "0. Wyjscie" << endl;
 	
 	cin >> wybor;
 	switch (wybor)
 	{
 	case 1:
 		cout << "Podaj od jakiego roku chcesz zobaczyc ksiazki: ";
-		cin
+		cin >> wybor_rok;
+		for(int i = 0; i < 7; i++){
+			if (ksiazki_DDT[i].rok >= wybor_rok){
+			cout << ksiazki_DDT[i].tytul << "	" << ksiazki_DDT[i].autor << "	" << ksiazki_DDT[i].autor << "	" << ksiazki_DDT[i].rok << "	" << ksiazki_DDT[i].wydawnictwo << endl;
+			}
+		}
+		
 			
 			
 		break;
 			
 	case 2:
-			
+			cout << "Podaj od jakiej ceny chcesz zobaczyc ksiazki: ";
+		cin >> wybor_cena;
+		for(int i = 0; i < 7; i++){
+			if (ksiazki_DDT[i].cena >= wybor_cena){
+			cout << ksiazki_DDT[i].tytul << "	" << ksiazki_DDT[i].autor << "	" << ksiazki_DDT[i].autor << "	" << ksiazki_DDT[i].rok << "	" << ksiazki_DDT[i].wydawnictwo << ksiazki_DDT[i].cena << endl;
+			}
+		}
 			
 		break;
 			
@@ -44,7 +59,11 @@ int main(){
 			
 			
 	case 3:
-			
+			cout << "Podaj od jakiego wydawnctwa chcesz zobaczyc ksiazki: ";
+		cin >> wybor_cena;
+		for(int i = 0; i < 7; i++){
+			if (ksiazki_DDT[i].wydawnictwo >= wybor_wydawnictwo){
+			cout << ksiazki_DDT[i].tytul << "	" << ksiazki_DDT[i].autor << "	" << ksiazki_DDT[i].autor << "	" << ksiazki_DDT[i].rok << "	" << ksiazki_DDT[i].wydawnictwo << endl;
 			
 		break;
 		
